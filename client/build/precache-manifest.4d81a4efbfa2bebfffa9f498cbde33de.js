9aedc2afc7c8b4190576552f5acd5c24bfdde30c
e-leal <leal.elyssa03@gmail.com>
2020-11-01 11:20:19 -0600

    corrected graphql reference

---
+++ client/build/precache-manifest.4d81a4efbfa2bebfffa9f498cbde33de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c406553c9a2668f8ad4aadfaf54a77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b7b7bb318f8c467fe6d",
+    "url": "/static/css/2.99a2b36c.chunk.css"
+  },
+  {
+    "revision": "74addf7bb99c1fb4888e",
+    "url": "/static/css/main.8ed30679.chunk.css"
+  },
+  {
+    "revision": "3b7b7bb318f8c467fe6d",
+    "url": "/static/js/2.972d30bc.chunk.js"
+  },
+  {
+    "revision": "857d129e4a5f4f33c7a4a143bf79109c",
+    "url": "/static/js/2.972d30bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74addf7bb99c1fb4888e",
+    "url": "/static/js/main.77ed11ed.chunk.js"
+  },
+  {
+    "revision": "95ab83a4221008eaadda",
+    "url": "/static/js/runtime-main.1dd9f351.js"
+  },
+  {
+    "revision": "63b39416462890311c083caf4568c29a",
+    "url": "/static/media/profile-placeholder.63b39416.png"
+  }
+]);
