d523ddc0f83d846d2f1cfc2eb52d15296c0b368f
e-leal <leal.elyssa03@gmail.com>
2020-11-01 18:21:14 -0600

    changed service worker to match with find from researching

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -1,108 +1,123 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/md5.js');
+importScripts('https://cdn.jsdelivr.net/npm/idb-keyval@3/dist/idb-keyval-iife.min.js');
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+var CACHE_NAME = 'main-cache-v1';
+var urlsToCache = [
+  // '/index.html',
+];
 
-importScripts(
-  "/precache-manifest.a7b88366b01d944e810bcaf70b0a27c9.js"
+// Init indexedDB using idb-keyval, https://github.com/jakearchibald/idb-keyval
+const store = new idbKeyval.Store('GraphQL-Cache', 'PostResponses');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+// Workbox with custom handler to use IndexedDB for cache.
+workbox.routing.registerRoute(
+  new RegExp('/graphql(/)?'),
+  // Uncomment below to see the error thrown from Cache Storage API.
+  //workbox.strategies.staleWhileRevalidate(),
+  async ({
+    event
+  }) => {
+    return staleWhileRevalidate(event);
+  },
+  'POST'
 );
 
-const ignored = self.__WB_MANIFEST;
+/*
+// When installing SW.
+self.addEventListener('install', (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+*/
 
-self.addEventListener('install', function (e){
-  e.waitUntil(
-      caches.open(CACHE_NAME).then(function (cache){
-          console.log('installing cache : ' + CACHE_NAME)
-          return cache.addAll(FILES_TO_CACHE)
-      })
-  )
-})
-
-self.addEventListener('activate', function (e){
-  e.waitUntil(
-      caches.keys().then(keyList => {
-          return Promise.all(
-              keyList.map(key => {
-                  if(key !== CACHE_NAME && key !== DATA_CACHE_NAME){
-                      console.log('Removing old cache data', key);
-                      return caches.delete(key);
-                  }
-              })
-          );
-      })
-  )
-})
-
-self.addEventListener('fetch', function (e){
-  if (e.request.url.includes('/api/')) {
-      e.respondWith(
-        caches
-          .open(DATA_CACHE_NAME)
-          .then(cache => {
-            return fetch(e.request)
-              .then(response => {
-                // If the response was good, clone it and store it in the cache.
-                if (response.status === 200) {
-                  cache.put(e.request.url, response.clone());
-                }
-  
-                return response;
-              })
-              .catch(err => {
-                // Network request failed, try to get it from the cache.
-                return cache.match(e.request);
-              });
-          })
-          .catch(err => console.log(err))
-      );
-  
-      return;
-    }
-  
-    e.respondWith(
-      fetch(e.request).catch(function() {
-        return caches.match(e.request).then(function(response) {
-          if (response) {
-            return response;
-          } else if (e.request.headers.get('accept').includes('text/html')) {
-            // return the cached home page for all requests for html pages
-            return caches.match('/');
-          }
-        });
-      })
-    );
-})
-
-
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
+// Return cached response when possible, and fetch new results from server in
+// the background and update the cache.
+self.addEventListener('fetch', async (event) => {
+  if (event.request.method === 'POST') {
+    event.respondWith(staleWhileRevalidate(event));
   }
+
+  // TODO: Handles other types of requests.
 });
 
-workbox.core.clientsClaim();
+async function staleWhileRevalidate(event) {
+  let promise = null;
+  let cachedResponse = await getCache(event.request.clone());
+  let fetchPromise = fetch(event.request.clone())
+    .then((response) => {
+      setCache(event.request.clone(), response.clone());
+      return response;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  return cachedResponse ? Promise.resolve(cachedResponse) : fetchPromise;
+}
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+async function serializeResponse(response) {
+  let serializedHeaders = {};
+  for (var entry of response.headers.entries()) {
+    serializedHeaders[entry[0]] = entry[1];
+  }
+  let serialized = {
+    headers: serializedHeaders,
+    status: response.status,
+    statusText: response.statusText
+  };
+  serialized.body = await response.json();
+  return serialized;
+}
 
+async function setCache(request, response) {
+  var key, data;
+  let body = await request.json();
+  let id = CryptoJS.MD5(body.query).toString();
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
-});
+  var entry = {
+    query: body.query,
+    response: await serializeResponse(response),
+    timestamp: Date.now()
+  };
+  idbKeyval.set(id, entry, store);
+}
+
+async function getCache(request) {
+  let data;
+  try {
+    let body = await request.json();
+    let id = CryptoJS.MD5(body.query).toString();
+    data = await idbKeyval.get(id, store);
+    if (!data) return null;
+
+    // Check cache max age.
+    let cacheControl = request.headers.get('Cache-Control');
+    let maxAge = cacheControl ? parseInt(cacheControl.split('=')[1]) : 3600;
+    if (Date.now() - data.timestamp > maxAge * 1000) {
+      console.log(`Cache expired. Load from API endpoint.`);
+      return null;
+    }
+
+    console.log(`Load response from cache.`);
+    return new Response(JSON.stringify(data.response.body), data.response);
+  } catch (err) {
+    return null;
+  }
+}
+
+async function getPostKey(request) {
+  let body = await request.json();
+  return JSON.stringify(body);
+}
